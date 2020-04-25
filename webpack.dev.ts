db9c535cda1d624df6f41adc565dd47156064be9
Abdelrhman Magdy <a-abmag@microsoft.com>
2020-04-25 05:09:45 +0200

    :sparkles: project scaffolding

---
+++ webpack.dev.ts
@@ -0,0 +1,24 @@
+import * as webpack from "webpack";
+import * as HtmlWebPackPlugin from "html-webpack-plugin";
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html"
+});
+
+const config: webpack.Configuration = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+    ]
+  },
+  plugins: [htmlPlugin],
+  devtool: 'source-map'
+};
+
+export default config;
